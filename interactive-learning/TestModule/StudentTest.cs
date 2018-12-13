094034f2a1336bce7e036a4ecc705ec157c23fed
DESKTOP-F9IPFTE\Bog <bgaidur@gmail.com>
2018-12-13 18:57:19 +0200

    Added test project
    Added test class for student and one method test.

---
+++ interactive-learning/TestModule/StudentTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DataLayer;
+using BusinessLayer;
+
+namespace TestModule
+{
+    [TestClass]
+    public class StudentTest
+    {
+        private Student _Student;
+
+        [TestInitialize]
+        private void TestInitializer()
+        {
+            _Student = new Student("Teo", "Ploae", ".NET");
+
+        }
+
+        [TestMethod] 
+        private void When_StudentIsInitialized_IsCreatedSuccessfully()
+        {
+            Assert.IsTrue(_Student.FirstName == "Teo");
+        }
+
+    }
+
+}
