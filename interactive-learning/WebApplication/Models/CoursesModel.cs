65cbe29932650970c1c3c62f34f831f19cba8d42
chircancristi <cristianchircan_Raw2008@yahoo.com>
2018-12-20 22:28:34 +0200

    Testing

---
+++ interactive-learning/WebApplication/Models/CoursesModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using DataLayer;
+using BusinessLayer;
+
+namespace Models
+{
+    public class CoursesModel
+    {
+        private readonly UnitOfWork _unitOfWork;
+
+        public CoursesModel()
+        {
+            _unitOfWork = new UnitOfWork();
+        }
+
+        public Course GetCourse(Guid guid)
+        {
+            return _unitOfWork.CourseRepository.GetCourseById(guid);
+        }
+
+        public Room GetRoom(Guid guid)
+        {
+            return _unitOfWork.RoomRepository.GetRoomById(guid);
+        }
+    }
+}
---
+++ interactive-learning/WebApplication/Models/InteractionModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using DataLayer;
+using BusinessLayer;
+
+namespace Models
+{
+    public class InteractionModel
+    {
+        private readonly UnitOfWork _unitOfWork;
+
+        public InteractionModel()
+        {
+            _unitOfWork = new UnitOfWork();
+        }
+
+        public Question GetQuestion(Guid guid)
+        {
+            return _unitOfWork.QuestionRepository.GetQuestionById(guid);
+        }
+
+        public Answer GetAnswer(Guid guid)
+        {
+            return _unitOfWork.AnswerRepository.GetAnswerById(guid);
+        }
+    }
+}
