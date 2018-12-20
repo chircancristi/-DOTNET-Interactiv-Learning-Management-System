2fdafaf279247baa3040611302d7fc19896f6ab5
chircancristi <cristianchircan_Raw2008@yahoo.com>
2018-12-20 20:33:08 +0200

    Prepared database testing

---
+++ interactive-learning/TestModule/DataBaseTests/DatabaseTests.cs
@@ -0,0 +1,58 @@
+﻿using BusinessLayer;
+using DataLayer;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace TestModule.DataBaseTests
+{
+    class DatabaseTests
+    {
+        PeopleContext peopleContext = new PeopleContext();
+        InteractionContext interactionContext = new InteractionContext();
+        CoursesContext coursesContext = new CoursesContext();
+        DatabaseTests()
+        {
+            var MyUnitOfWork = new UnitOfWork(peopleContext, interactionContext, coursesContext);
+
+            var student1 = new Student("Alex", "Stoica", "Retele");
+            var student2 = new Student("Tudor", "Melnic", "Franceza");
+
+            var profesor = new Profesor("Valeriu", "Mardare", "Franceza");
+
+            var course = new Course("Dotnet", profesor.Id);
+
+            var room = new Room(course.Id, profesor.Id);
+
+            var question1 = new Question(student1.Id, room.Id, "intrebare desteapta", "Se da cursul asta in sesiune?");
+
+            var question2 = new Question(student2.Id, room.Id, "alta intrebare desteapta", "Putem pleca?");
+
+            var answer1 = new Answer(profesor.Id, question1.Id, "Nu");
+            var answer2 = new Answer(profesor.Id, question2.Id, "Nu");
+
+            MyUnitOfWork.StudentRepository.Add(student1);
+            MyUnitOfWork.StudentRepository.Add(student2);
+
+            MyUnitOfWork.ProfesorRepository.Add(profesor);
+
+            MyUnitOfWork.CourseRepository.Add(course);
+            MyUnitOfWork.RoomRepository.Add(room);
+
+            MyUnitOfWork.QuestionRepository.Add(question1);
+            MyUnitOfWork.QuestionRepository.Add(question2);
+
+            MyUnitOfWork.AnswerRepository.Add(answer1);
+            MyUnitOfWork.AnswerRepository.Add(answer2);
+
+            MyUnitOfWork.Commit();
+        }
+        /*
+        [Fact]
+        private void TestsIfStudentExistsInDatabase()
+        {
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsTrue(peopleContext.id == "Bine");
+        }*/
+    }
+}
