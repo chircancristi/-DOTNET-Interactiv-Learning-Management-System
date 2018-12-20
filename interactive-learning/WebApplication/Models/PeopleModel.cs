3bdfbe7c6bd27ad0ffd57ee3af2e868350c0ed6d
DESKTOP-F9IPFTE\Bog <bgaidur@gmail.com>
2018-12-20 22:19:06 +0200

    Created 3 models

---
+++ interactive-learning/WebApplication/Models/PeopleModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using DataLayer;
+using BusinessLayer;
+
+namespace Models
+{
+    public class PeopleModel
+    {
+        private readonly UnitOfWork _unitOfWork;
+
+        public PeopleModel()
+        {
+            _unitOfWork = new UnitOfWork();
+        }
+
+        public Student GetStudent(Guid guid)
+        {
+            return _unitOfWork.StudentRepository.GetStudentById(guid);
+        }
+
+        public Profesor GetProfesor(Guid guid)
+        {
+            return _unitOfWork.ProfesorRepository.GetProfesorById(guid);
+        }
+    }
+}
