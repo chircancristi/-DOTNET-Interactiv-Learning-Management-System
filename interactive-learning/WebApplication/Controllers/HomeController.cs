9e39984fcaae357374900c6ea08889d0b75eba0b
chircancristi <cristianchircan_Raw2008@yahoo.com>
2019-01-07 20:58:07 +0200

    Add question functionality implemented

--- interactive-learning/WebApplication/Controllers/HomeController.cs
+++ interactive-learning/WebApplication/Controllers/HomeController.cs
@@ -14,56 +14,65 @@ namespace WebApplication.Controllers
         CoursesModel courses = new CoursesModel();
         PeopleModel people = new PeopleModel();
         Profesor profesor = new Profesor();
-        Guid id;
-        Course course;
+        Guid id = Guid.Parse("E0C3AC4D-6B9C-42F4-917C-FB307C6A652E");
+        Course course = new Course();
+        List<Student> students = new List<Student>();
+        List<Question> questions = new List<Question>();
+        List<String> ownersName = new List<String>();
+
         public IActionResult Professor()
         {
-            id = Guid.Parse("E0C3AC4D-6B9C-42F4-917C-FB307C6A652E");
-            this.generateProfessor(id);
-            List<Student> students = new List<Student>();
-            List<Question> questions = new List<Question>();
-            List<String> ownersName = new List<String>();
-            String firstName, lastName,fullName;
+            
+            this.GenerateProfessor();
+            SetStudents();
+           
+            @ViewBag.students = students;
+            @ViewBag.questions = questions;
+            @ViewBag.owners = ownersName;
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Professor(string questionProfessor)
+        {
+            this.GenerateProfessor();
+            Question question = new Question(this.id, this.course.GeneralRoomId, "professor", questionProfessor);
+            
+            interaction.AddQuestion(question);
+            SetStudents();
+            @ViewBag.students = this.students;
+            @ViewBag.questions = this.questions;
+            @ViewBag.owners = this.ownersName;
+            return View();
+        }
+        private void GenerateProfessor()
+        {
+            
+            profesor = people.GetProfesor(this.id);
+            this.course = courses.GetCourse(profesor.CourseId);
 
+        }
+        public void SetStudents()
+        {
+            String firstName, lastName, fullName;
             students = courses.GetStudentsByCourse(this.course.Id);
             questions = interaction.GetQuestionsByRoomId(this.course.GeneralRoomId);
-            
-
+        
             for (int i = 0; i < questions.Count; i++)
             {
+
                 if (questions[i].Type.Equals("professor"))
                 {
                     firstName = people.GetProfesor(questions[i].OwnerId).FirstName;
-                    lastName  = people.GetProfesor(questions[i].OwnerId).LastName;
+                    lastName = people.GetProfesor(questions[i].OwnerId).LastName;
                 }
                 else
                 {
                     firstName = people.GetStudent(questions[i].OwnerId).FirstName;
                     lastName = people.GetStudent(questions[i].OwnerId).LastName;
                 }
-                fullName = firstName+" " + lastName;
+                fullName = firstName + " " + lastName;
                 ownersName.Add(fullName);
             }
-           
-            @ViewBag.students = students;
-            @ViewBag.questions = questions;
-            ViewBag.owners = ownersName;
-            return View();
-        }
-        [HttpPost]
-        public IActionResult questionProfessor(string questionProfessor)
-        {
-            Question question = new Question(this.id, this.course.GeneralRoomId, "professor", questionProfessor);
-            
-            interaction.AddQuestion(question);
-            return View();
-        }
-        private void generateProfessor(Guid id)
-        {
-            Guid profesorId = id;
-            profesor = people.GetProfesor(profesorId);
-            this.course = courses.GetCourse(profesor.CourseId);
-
         }
 
         public IActionResult Privacy()
