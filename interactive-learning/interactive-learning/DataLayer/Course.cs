3f3cc628ce3159da8742d30aa29c324c9d279b06
anggbard <gabrielangel.ardeleanu@gmail.com>
2019-01-07 13:29:14 +0200

    added the concept of general room + minor changes to tabels

--- interactive-learning/interactive-learning/DataLayer/Course.cs
+++ interactive-learning/interactive-learning/DataLayer/Course.cs
@@ -6,16 +6,13 @@ namespace DataLayer
         public Guid Id { get; private set; }
         public string Name { get; private set; }
         public Guid ProfessorId { get; private set; }
-        public DateTime BeginAt { get; private set; }
-        public DateTime EntAt { get; private set; }
+        public Room GeneralRoom { get; private set; }
 
-        public Course(string name, Guid professorId)
-        {
+        public Course(string name, Guid professorId) {
             Id = Guid.NewGuid();
             Name = name;
             ProfessorId = professorId;
-            BeginAt = DateTime.Now.Date;
-            EntAt = DateTime.Now.Date;
+            GeneralRoom = new Room(Id, ProfessorId);
         }
     }
 }
